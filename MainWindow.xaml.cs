629c9511adba2f49607410a6b7fffb6b6671152b
Antti <anttiparhiala@hotmail.com>
2023-08-18 11:49:44 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JasenrekisteriNoSQL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly Register register = new();
+        readonly AddMember addMember = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Page.NavigationService.Navigate(register);
+        }
+
+        private void CreateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Page.NavigationService.Navigate(addMember);
+        }
+
+        private void RegisterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Page.NavigationService.Navigate(register);
+        }
+    }
+}
---
+++ Member.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JasenrekisteriNoSQL
+{
+    public class Member
+    {
+
+        public Member(string etunimi, string sukunimi, string osoite, string postinumero,
+            string puhelin, string sahkoposti, DateTime jasenyydenAlkuPvm) 
+        {
+            Etunimi = etunimi;
+            Sukunimi = sukunimi;
+            Osoite = osoite;
+            Postinumero = postinumero;
+            Puhelin = puhelin;
+            Sahkoposti = sahkoposti;
+            JasenyydenAlkuPvm = jasenyydenAlkuPvm;
+        }
+
+        public string Etunimi {get; set; }
+
+        public string Sukunimi { get; set; }
+
+        public string Osoite { get; set; }
+
+        public string Postinumero { get; set; }
+
+        public string Puhelin { get; set; }
+
+        public string Sahkoposti { get; set; }
+
+        public DateTime JasenyydenAlkuPvm { get; set; }
+
+
+
+    }
+}
